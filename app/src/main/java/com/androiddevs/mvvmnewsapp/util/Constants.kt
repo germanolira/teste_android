8de77e06bd7757b4da1b75acc382973c2137c1c9
Germano Lira <dolan.3@hotmail.com>
2021-08-25 20:48:38 -0300

    adding and deleting articles to our room database

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "0b19cf22c8cb485eab41c4006366640a"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
